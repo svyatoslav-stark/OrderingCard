35d09b50271f511afc212987e538b55defa578d2
Svyatoslav Starkov <svyatoslav.starkov@yandex.ru>
2025-06-16 18:57:01 +0400

    Сhanges OrderingCardTest

--- src/test/java/OrderingCardTest.java
+++ src/test/java/OrderingCardTest.java
@@ -22,7 +22,7 @@ public class OrderingCardTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
